248fefce44b86a526ac9899d80088baef7e1cbbd
Nguyễn Gia Vỉ <vinguyengia60@gmail.com>
2026-05-14 03:08:25 +0700

    fix loi

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 const nextConfig: NextConfig = {
   outputFileTracingRoot: currentDir,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
